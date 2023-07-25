13b7a9a50cbe7c9a2830ef2d818f725e6497b1e2
Shwetank Singh <ssingh.21.2019@gmail.com>
2023-07-25 19:22:47 +0530

    uploads01

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour {
+
+    bool alive = true;
+    
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] float jumpSpeed = 1000f;
+    public float speed = 5;
+    [SerializeField] Rigidbody rb;
+
+    float horizontalInput;
+    [SerializeField] float horizontalMultiplier = 2;
+
+    public float speedIncreasePerPoint = 0.1f;
+
+    private void FixedUpdate()
+    {
+        if (!alive) return;
+
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+
+       
+    }
+    private void Update () {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (transform.position.y < -5) {
+            Die();
+        }
+	}
+
+    public void Die ()
+    {
+        alive = false;
+        // Restart the game
+        Invoke("Restart", 2);
+    }
+
+    void Restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Jump()
+    {
+        float height = GetComponent<Collider>().bounds.size.y;
+        bool isGrounded = Physics.Raycast(transform.position,Vector3.down,(height/2)+0.1f,groundMask);
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpSpeed);
+        }
+        
+    }
+
+    
+}
